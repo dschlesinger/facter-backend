938acb8f3b59aa254288a10f6eab070aebc7f1d3
dschlesinger <89662251+dschlesinger@users.noreply.github.com>
2023-11-12 17:02:10 -0500

    Co-authored-by: Oleg Polin <olegpolin@users.noreply.github.com> Co-authored-by: Joshua Lee <scaryjoshie@users.noreply.github.com>

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 # Python imports
 from python_util.extract_article import extract
-from python_util.model import get_classifier
+from python_util.model import get_classifier,get_tone_analysis,get_tone_screen
+import numpy as np
 
 # Set input classes
 class URL(BaseModel):
@@ -15,12 +16,26 @@ class Article(BaseModel):
 # Creates classifier object
 classifier = get_classifier()
 
+tone_analyzer =  get_tone_analysis()
+
+tone_screener =  get_tone_screen()
+
 #Finds mean score based on model predictions
-def process(data):
+def process_bias(data):
     total_score = 0
 
     for i in data:
         if i[0]["label"] == "Biased":
+            total_score -= i[0]["score"]
+        else:
+            total_score += i[0]["score"]
+    return total_score/len(data)
+
+def process_tone(data):
+    total_score = 0
+
+    for i in data:
+        if i[0]["label"] == "NEGATIVE":
             total_score -= i[0]["score"]
         else:
             total_score += i[0]["score"]
@@ -38,7 +53,9 @@ async def root():
 async def predict_smth(text: Article):
     try:
         #store predictions
-        detect = []
+        detect_bias = []
+        detect_tone = []
+        detect_screen = []
 
         #split into processable chunks
         info = text.text.split("\n")
@@ -52,18 +69,20 @@ async def predict_smth(text: Article):
 
         #run model over each chunk
         for part in info:
-            detect.append(classifier(part))
+            detect_bias.append(classifier(part))
+            detect_tone.append(tone_analyzer(part))
 
-            print(f"{part}|{detect}")
+            print(f"{part}|{detect_bias}")
 
 
         #generates final score
-        final_score = process(detect)
+        final_score = process_bias(detect_bias)
 
+        final_tone_score = process_tone(detect_tone)
 
         #returns final score + list for highlights
         return [
-            final_score, info, detect, "Custom Input"
+            final_score, info, detect_bias, detect_tone, final_tone_score, "Custom Input"
         ]
     except:
         return [
@@ -73,49 +92,52 @@ async def predict_smth(text: Article):
 #prediction root
 @app.post("/analyze/url/")
 async def predict_smth(url: URL):
+    #try:
+    #store predictions
+    detect_bias = []
+    detect_tone = []
+    detect_screen = []
+
+    #extract article from url
     try:
-        #store predictions
-        detect = []
-    
-        #extract article from url
-        try:
-            article = extract(url.url)
-        except:
-            return [
-                0, [], [], "Error: Invalid URL"
-            ]
-
-        #split into processable chunks
-        info = article.text.split("\n")
-
-        #removes blanks
-        while("" in info):
-            info.remove("")
-
-        if len(info) == 0:
-                return [
-                0, [], [], "Error: No article found for the URL"
-            ]
-
-        #for debuging extract
-        #print(info)
-
-        #run model over each chunk
-        for part in info:
-            detect.append(classifier(part))
-
-            print(f"{part}|{detect}")
-
-
-        #generates final score
-        final_score = process(detect)
-
-    
-        #returns final score + list for highlights
-        return [
-            final_score, info, detect, article.title
-        ]
+        article = extract(url.url)
     except:
         return [
-                    0, [], [], "Error: Server error, try again"
-                ]
+           0, [], [], "Error: Invalid URL"
+        ]
+
+    #split into processable chunks
+    info = article.text.split("\n")
+
+    #removes blanks
+    while("" in info):
+        info.remove("")
+
+    if len(info) == 0:
+            return [
+            0, [], [], "Error: No article found for the URL"
+        ]
+
+    #for debuging extract
+    #print(info)
+
+    #run model over each chunk
+    for part in info:
+        detect_bias.append(classifier(part))
+        detect_tone.append(tone_analyzer(part))
+
+        print(f"{part}|{tone_analyzer(part)}")
+
+    #generates final score
+    final_score = process_bias(detect_bias)
+
+    final_tone_score = process_tone(detect_tone)
+
+    #returns final score + list for highlights
+    return [
+        final_score, info, detect_bias, detect_tone, final_tone_score, article.title
+    ]
+    # except:
+    #     return [
+    #                 0, [], [], "Error: Server error, try again"
+    #             ]
--- python_util/model.py
+++ python_util/model.py
@@ -3,9 +3,23 @@ from transformers import AutoTokenizer, TFAutoModelForSequenceClassification
 from transformers import pipeline
 
 # Creates tokenizer and model
-tokenizer = AutoTokenizer.from_pretrained("d4data/bias-detection-model")
-model = TFAutoModelForSequenceClassification.from_pretrained("d4data/bias-detection-model")
+tokenizer_bias = AutoTokenizer.from_pretrained("d4data/bias-detection-model")
+model_bias = TFAutoModelForSequenceClassification.from_pretrained("d4data/bias-detection-model")
+
+tokenizer_tone = AutoTokenizer.from_pretrained("distilbert-base-uncased-finetuned-sst-2-english")
+model_tone = TFAutoModelForSequenceClassification.from_pretrained("distilbert-base-uncased-finetuned-sst-2-english")
+
+model_name = "Recognai/zeroshot_selectra_medium"
+tokenizer_screen = AutoTokenizer.from_pretrained(model_name)
+model_screen = TFAutoModelForSequenceClassification.from_pretrained(model_name)
+
 
 # Function to return classifier model for bias
 def get_classifier():
-    return pipeline('text-classification', model=model, tokenizer=tokenizer) # cuda = 0,1 based on gpu availability
+    return pipeline('text-classification', model=model_bias, tokenizer=tokenizer_bias) # cuda = 0,1 based on gpu availability
+
+def get_tone_analysis():
+    return pipeline('text-classification', model=model_tone, tokenizer=tokenizer_tone)
+
+def get_tone_screen():
+    return pipeline('text-classification', model=model_screen, tokenizer=tokenizer_screen)
